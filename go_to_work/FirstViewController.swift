6683a59da0879f576a93f16833c24d4794726e7c
y-okagawa <yuya.okagawa@gmail.com>
2019-11-17 10:47:17 +0900

    Initial Commit

---
+++ go_to_work/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  go_to_work
+//
+//  Created by Okagawa Yuya on 2019/11/17.
+//  Copyright © 2019 Okagawa Yuya. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
